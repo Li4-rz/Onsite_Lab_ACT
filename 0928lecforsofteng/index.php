67615c1132a497eacb0e05543a5af65799b9697d
Li4-rz(1909-Mark) <159553600+Li4-rz@users.noreply.github.com>
2024-10-06 12:08:18 +0800

    Update index.php

--- 0928lecforsofteng/index.php
+++ 0928lecforsofteng/index.php
@@ -22,6 +22,47 @@
     </style>
 </head>
 <body>
+    
+    <?php
+        //Php statement for No.5 Insertion of Records in Database
+        /*$QUERY = "INSERT INTO s_employees (Employee_Id, First_Name, Last_Name, Role VALUES (?,?,?,?))";
+
+        $stmt = $pdo->prepare($QUERY);
+
+        $executeQuery = $stmt->execute([31, "P.", "Diddy", "Salesperson"]);
+
+        if($ececuteQuery) {
+                echo "Query Successfull!"
+        } else {
+            echo "Query Failed."*/
+
+        //Php statement fr No.6 Deletion of Records in Database
+        /*$QUERY = "DELETE FROM s_employees WHERE Employee_id = 31";
+
+        $stmt = $pdo->prepare($QUERY);
+
+        $executeQuery = $stmt->execute();
+
+        if($ececuteQuery) {
+                echo "Query Successfull!"
+        } else {
+            echo "Query Failed."*/
+        
+        //Php statment for No.7 Updating Records of Database
+        /*$QUERY = "UPDATE s_employees SET First_Name = ?, Last_Name = ? WHERE Employee_id = 1";
+
+        $stmt = $pdo->prepare($QUERY);
+
+        $executeQuery = $stmt->execute(["LeFlop", "James"]);
+
+        if($ececuteQuery) {
+                echo "Update Successfull!"
+        } else {
+            echo "Query Failed."
+        }*/
+    ?>
+    <!-- Table for No.8 -->
+    <!-- 
     <h1>Sales Emplyees Table</h1>
     <table>
         <thead>
@@ -33,7 +74,9 @@
             </tr>
         </thead>
         <tbody>
+    
             <?php
+                //Php statement for No.8 putting database into HTML Values
                 $sql = "SELECT Employee_id, First_Name, Last_Name, Role FROM s_employees";
                 $stmt = $pdo->query($sql);
                 $employees = $stmt->fetchAll();
@@ -47,6 +90,6 @@
                 </tr>
             <?php endforeach; ?>    
         </tbody>
-    </table>
+    </table> -->
 </body>
 </html>
