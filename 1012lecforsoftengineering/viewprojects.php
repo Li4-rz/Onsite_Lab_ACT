6d195615aecfafc0989d974d3efa9cc5105eb6a2
Mark Pasuquin <159553600+Li4-rz@users.noreply.github.com>
2024-10-29 15:04:48 +0800

    Update viewprojects.php

--- 1012lecforsoftengineering/viewprojects.php
+++ 1012lecforsoftengineering/viewprojects.php
@@ -99,6 +99,7 @@
             <th>Project ID</th>
             <th>Project Name</th>
             <th>Technologies Used</th>
+            <th>Date Added</th>
             <th>Project Owner</th>
             <th>Action</th>
         </tr>
@@ -109,6 +110,7 @@
             <td><?php echo $row['project_name']; ?></td>
             <td><?php echo $row['technologies_used']; ?></td>
             <td><?php echo $row['date_added']; ?></td>
+            <td><?php echo $row['project_owner']; ?></td>
             <td>
                 <a href="editproject.php?project_id=<?php echo $row['project_id']; ?>&web_dev_id=<?php echo $_GET['web_dev_id']; ?>">Edit</a>
                 <a href="deleteproject.php?project_id=<?php echo $row['project_id']; ?>&web_dev_id=<?php echo $_GET['web_dev_id']; ?>">Delete</a>
