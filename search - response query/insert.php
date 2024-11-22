9c4f7caadd82983f2dc39126930ddfef878a8b1f
Mark Pasuquin <159553600+Li4-rz@users.noreply.github.com>
2024-11-22 10:53:51 +0800

    Update insert.php

--- search - response query/insert.php
+++ search - response query/insert.php
@@ -21,7 +21,7 @@ if ($id) {
 	<link rel="stylesheet" href="styles.css">
 </head>
 <body>
-	<h1><?php echo $id ? "Edit Teacher" : "Add Teacher"; ?>!</h1>
+	<h1><?php echo $id ? "Edit Applicant" : "Add Applicant"; ?>!</h1>
 	<form action="core/handleForms.php" method="POST">
 		<p>
 			<label for="firstName">First Name</label>
@@ -40,13 +40,13 @@ if ($id) {
 			<input type="text" name="gender" value="<?php echo $id ? $id['gender'] : ''; ?>">
 		</p>
 		<p>
-			<label for="subjectSpecialty">Subject Specialty</label>
+			<label for="subjectSpecialty">nationality</label>
 			<input type="text" name="nationality" value="<?php echo $id ? $id['nationality'] : ''; ?>">
 		</p>
 		<p>
 			<!-- Hidden input to track teacher ID for editing -->
 			<?php if ($id): ?>
-				<input type="hidden" name="teacher_id" value="<?php echo $id['id']; ?>">
+				<input type="hidden" name="id" value="<?php echo $id['id']; ?>">
 			<?php endif; ?>
 			<input type="submit" name="editUserBtn" value="<?php echo $id ? "Save Changes" : "Add Applicant"; ?>">
 		</p>
